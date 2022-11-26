3b7cfa278211b14d70ebed86076ca70a3321c228
Lam Theo <theo.lam@etu.univ-orleans.fr>
2022-11-26 12:50:02 +0100

    Ajout BDD MariaDB

---
+++ src/main/java/test/Bdd.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Bdd {
+
+
+
+    public void test() throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/test",
+                "root",
+                "root");
+
+        System.out.println("Connexion réussie");
+    }
+
+}
